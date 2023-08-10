180dc6507c185fb8b307a0dff5d50749fd6ca999
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-10 08:24:31 -0700

    putchar

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 1 Successful
+*/
+int main(void)
+{
+char x = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n"
+putchar(x);
+return (1);
+}
