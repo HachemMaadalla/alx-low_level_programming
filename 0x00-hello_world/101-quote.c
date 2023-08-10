daa972ce223ce6d1409ddb71e81ad020334124b6
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-10 08:27:57 -0700

    putchar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-char x = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+char x[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 putchar(x);
 return (1);
 }
