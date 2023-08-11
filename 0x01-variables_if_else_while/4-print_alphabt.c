0260752dda85ccd7b45101258bd34347912aefe7
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 10:45:31 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+char x;
+for (x = 'a'; x <= 'z'; x++)
+if (x != 'q' && x != 'e')
+{	
+	putchar(x);
+}
+putchar('\n');
+return (0);
+}
