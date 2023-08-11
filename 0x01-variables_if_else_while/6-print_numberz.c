a1c7e7549456b6c4ebd9d2539b90a09c1f358dd5
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 10:55:47 -0700

    numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+    int x;
+    for (x = 0 ; x < 10 ; x++)
+    {
+        putchar(x + '0');
+    }
+    putchar('\n');
+    return (0);
+}
