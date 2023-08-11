2d2dd8ca83b4edfd9e241b81873eeb4914f93e1b
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 07:08:20 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+char x[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+putchar(x[]);
+return (0);
+}
