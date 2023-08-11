7f579172e7e7573929f58c72cf4590c875b4497b
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 07:25:13 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,8 +7,9 @@
 int main(void)
 {
 char x[] = "abcdefghijklmnopqrstuvwxyz";
-for (int i = 0; x[i] != '\0'; i++) {
-        putchar(x[i]);
-    }
+for (x = 'a'; x <= 'z'; x++)
+	putchar(letter);
+
+putchar('\n');
 return (0);
 }
