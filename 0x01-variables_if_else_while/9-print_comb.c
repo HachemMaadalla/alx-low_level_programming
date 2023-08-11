3f875479e92c8e5e07510cbf4a1710d1ac0867c0
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 12:33:28 -0700

    ascending

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+{
+putchar(x + '0');
+putchar(',');
+}
+
+printf("\n");
+return (0);
+}
