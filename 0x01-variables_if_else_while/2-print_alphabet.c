9175cea894efed5cfc3d641b363dc46464bf38e9
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 07:10:04 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -6,7 +6,7 @@
 */
 int main(void)
 {
-char x[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+char x['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 putchar(x[]);
 return (0);
 }
