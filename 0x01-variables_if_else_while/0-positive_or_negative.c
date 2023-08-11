de4b376747d8b9f9b2a00f1c2ca614a6d118e385
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 06:39:12 -0700

    is it positive or negative ?

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+int x = n;
+if ( x > 0)
+{
+printf("%d is positive",x);
+} elseif ( x == 0 )
+{
+printf("%d is zero",x);
+} else
+{
+printf("%d is negative",x);
+}
+return (0);
+}
