a568c73d55ac6b2b3e70efb3577617afa2710c2e
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-11 10:59:28 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - This is the entry point of the code
+*
+* Return: 0 Successful
+*/
+int main(void)
+{
+char x;
+for (x = 'z'; x <= 'a'; x--)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
