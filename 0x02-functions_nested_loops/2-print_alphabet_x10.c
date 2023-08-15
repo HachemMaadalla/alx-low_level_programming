e4edf1ba88483e7028afa0713c6b8bc788e43335
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-15 12:09:55 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,19 +5,12 @@
 *
 * Return: 0 Successful
 */
-int _putchar(char c)
-{
-return putchar(c);
-}
-
-int print_alphabet_x10(void)
+void print_alphabet_x10(void)
 {
 for (int i=0; i < 10 ; i++)
 {
 print_alphabet();
 }
-return (0);
-
 }
 
 int main(void)
