764ff68f743f1879e0f8da470909005375132ad8
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-17 12:13:37 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers 0-14 10 times
+ */
+void more_numbers(void)
+{
+int n, i;
+for (i = 0; i <= 9; i++)
+{
+	for (n = 0; n <= 14; n++)
+	{
+		if (n > 9)
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+	}
+_putchar('\n');
+}
+}
