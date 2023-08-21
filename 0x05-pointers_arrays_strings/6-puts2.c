1c629713d3da55bf82769bec86852bb698644e05
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-21 08:43:08 -0700

    6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+* @str : char str
+*/
+
+void puts2(char *str)
+{
+for (int i = 0; i < strlen(s); i += 2)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
