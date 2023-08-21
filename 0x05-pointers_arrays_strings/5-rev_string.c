56bf3f43235d78cb731c9eecdf02109b4e8e755c
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-21 00:28:17 -0700

    no comments

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ while (s[i++])
 {
 len++;
 }
-for (i = len - 1; i >= len / 2; i--) // we are swapping the first half of the string with  the second half of it
+for (i = len - 1; i >= len / 2; i--)
 {
 rev = s[i];
 s[i] = s[len - i - 1];
