7b9ca2329d8ff664ca60a7bfcea60d83079a3f2c
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-20 22:05:53 -0700

    reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,14 @@ void print_rev(char *s)
 {
 int len = 0;
 int i;
-while (s[i++])
-	len++;
+while (*s)
+{
+len++;
+s++;
+}
 for (i = len - 1; i >= 0; i--)
 {
 _putchar(s[i]);
 }
 _putchar('\n');
 }
-
