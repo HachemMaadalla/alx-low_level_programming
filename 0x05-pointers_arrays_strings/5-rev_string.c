c54a650df74f64e11030201e94f94e5c84b9de71
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-20 22:14:37 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* rev_string - reverses a string.
+* @s : char s
+*/
+
+void rev_string(char *s)
+{
+int len = 0, i = 0;
+char rev;
+while (s[i++])
+{
+len++;
+}
+for (i = len - 1; i >= len / 2; i--) // we are swapping the first half of the string with  the second half of it
+{
+rev = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = rev;
+}
+}
