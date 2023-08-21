3fdf07d567285351503f70e5f3a50efbf9d14468
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-20 21:15:18 -0700

    swaping

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* swap_int - waps the values of two integers. 
+* @a : integer 1
+* @b : integer 2
+*/
+
+void swap_int(int *a, int *b)
+{
+int stocking;
+stocking = *a;
+*a = *b;
+*b = stocking;
+}
