edca3331fa986a527fd25a7fa287003e9af69b18
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-29 23:27:59 -0700

    file 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,44 +1,42 @@
 #include "main.h"
-int _is_palindrome(char *start, char *end);
-char *_end(char *s);
 
 /**
  * _end - Finds the end of a string.
- * @s: char
+ * @s: The string whose end is to be found.
  *
- * Return: a pointer to the last character in the string.
+ * Return: A pointer to the null terminator of the string.
  */
 char *_end(char *s)
 {
-if (*s)
-return (_end(s + 1));
-return (s - 1);
+if (!*s)
+return s - 1;
+return _end(s + 1);
 }
 
 /**
- * _is_palindrome - Checks if a string is a palindrome.
- * @start: The start pointer 
- * @end: The end pointer 
+ * _is_palindrome - Recursively checks if a segment of a string is a palindrome.
+ * @start: Pointer to the start of the segment.
+ * @end: Pointer to the end of the segment.
  *
- * Return: 1 or 0
+ * Return: 1 if the segment is a palindrome, otherwise 0.
  */
 int _is_palindrome(char *start, char *end)
 {
 if (start >= end)
-return (1);
+return 1;
 if (*start != *end)
-return (0);
-return (_is_palindrome(start + 1, end - 1));
+return 0;
+return _is_palindrome(start + 1, end - 1);
 }
 
 /**
  * is_palindrome - Checks if a string is a palindrome.
- * @s: char
+ * @s: The string to be checked.
  *
- * Return: 1 or 0
+ * Return: 1 if the string is a palindrome, otherwise 0.
  */
 int is_palindrome(char *s)
 {
 char *end = _end(s);
-return (_is_palindrome(s, end));
+return _is_palindrome(s, end);
 }
