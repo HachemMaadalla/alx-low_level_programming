95b7351c1860dcdb0ad9d13e04b1a56ddb3dfbce
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-08-29 23:22:49 -0700

    file 8

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -8,19 +8,19 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0')
-	{
-	while (*s2 == '*')
-	s2++;
-	return (*s2 == '\0');
-	}
+if (*s1 == '\0')
+{
+if (*s2 == '*')
+return wildcmp(s1, s2 + 1);
+return (*s2 == '\0');
+}
 if (*s2 == '*')
 {
 return (wildcmp(s1 + 1, s2) || wildcmp(s1, s2 + 1));
 }
-	if (*s1 == *s2)
-	{
-	return (wildcmp(s1 + 1, s2 + 1));
-	}
+if (*s1 == *s2)
+{
+return (wildcmp(s1 + 1, s2 + 1));
+}
 return (0);
 }
