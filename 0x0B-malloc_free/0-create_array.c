312da9ad21612d4ebcd3d078b3f6c2775200ba04
HachemMaadalla <hachem.maadalla@hotmail.com>
2023-09-05 07:57:08 -0700

    fixing errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,7 @@ char *create_array(unsigned int size, char c)
 	char *array;
 	unsigned int i;
 array = malloc(sizeof(char) * size);
-if (array == NULL)
+if (size == 0 || array == NULL)
 return (NULL);
 for (i = 0; i < size; i++)
 array[i] = c;
